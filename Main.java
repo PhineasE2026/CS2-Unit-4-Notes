d9a80e88862de6430c7a1a027cb467bfd56696b6
finny <phineas.ehrlich2026@gbwl.org>
2024-11-21 13:55:20 +0000

    For while for loops

--- Main.java
+++ Main.java
@@ -28,5 +28,62 @@ public class Main {
          System.out.println(g);
          g = g * 1.5;
       }
+      String message = "Have a 1ong and happy 1ife";
+      int index = 0;
+
+      while (message.indexOf("1") >= 0)
+      {
+         // Find the next index for 1
+         index = message.indexOf("1");
+         System.out.println("Found a 1 at index: " + index);
+         // Replace the 1 with a l at index by concatenating substring up to
+         // index and then the rest of the string.
+         String firstpart = message.substring(0, index);
+         String lastpart = message.substring(index + 1);
+         message = firstpart + "l" + lastpart;
+         System.out.println("Replaced 1 with l at index " + index);
+         System.out.println("The message is currently " + message + " but we aren't done looping yet!");
+      }
+      System.out.println("Cleaned text: " + message);
+      String exm = "5unn1es h0p a 10t";
+      int f = 0;
+      while (f < 5) {
+         if (f <= 1) {
+            index = exm.indexOf("1");
+         }
+         else if (f == 2) {
+            index = exm.indexOf("5");
+         }
+         else {
+            index = exm.indexOf("0");
+         }
+         System.out.println("Found a number at index: " + index);
+         String firstHalf = exm.substring(0, index);
+         String secondHalf = exm.substring(index + 1);
+         if (f == 0) {
+            exm = firstHalf + "i" + secondHalf;
+         }
+         else if (f == 1) {
+            exm = firstHalf + "l" + secondHalf;
+         }
+         else if (f == 2) {
+            exm = firstHalf + "b" + secondHalf;
+         }
+         else {
+            exm = firstHalf + "o" + secondHalf;
+         }
+         System.out.println("Replaced number with correct letter");
+         System.out.println("The message is currently " + exm + " but we aren't done looping yet!");
+         f++;
+      }
+      System.out.println("Cleaned text: " + exm);
+      String xs = "Tomorrow we're going to Paris";
+      String xsr = "";
+      String ithLetter;
+      for (int i = 0; i < xs.length(); i++) {
+         ithLetter = xs.substring(i, i+1);
+         xsr = ithLetter + xsr;
+      }
+      System.out.println(xs + " reversed is " + xsr);
    }
 }
